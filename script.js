aebdb40fa24f44b6b30bdd4041d55cb91df312e0
Voyager56 <acroduke37@gmail.com>
2021-10-30 16:38:04 +0400

    init

---
+++ script.js
@@ -0,0 +1,37 @@
+function checkOdd(number){
+    if(number%2==1){
+        return true
+    }else{
+        return false
+    }
+}
+
+const getCurrencySymbolFromCode = function(currency){
+    const upperCaseCurrency = currency.toUpperCase();
+    switch(upperCaseCurrency){
+        case `USD`: return '$'
+        case `EUR`: return '€'
+        case `GEL`: return 'ლ'
+        default: return `${upperCaseCurrency}`
+    }
+}
+
+const lowerCase = (string) => string.toLowerCase();
+
+let array = [1,2,3,4,5,6,7,8,9,10];
+const evenArray = (array) => array.filter((x)=> x%2==0);
+console.log(evenArray(array))
+
+const objectSearch = (objectArray, string) => objectArray.find(({name}) => name === string);
+
+
+const cloudGame = function(c, k){
+    let n = c.length;
+    let currentStep = 0;
+    let currentEnergy = 100;
+    do{
+        currentEnergy -= (c[currentStep] === 0)? 1:3;
+        currentStep = (currentStep + k ) % n;
+    }while(currentStep!=0)
+    return currentEnergy;
+}
